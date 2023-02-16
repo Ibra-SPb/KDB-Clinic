da06cfe0f471783e9fce4f4560a5ea6d311af388
Ibra <Ibrahim-1991@mail.ru>
2023-02-16 17:20:43 +0300

    init backend packages

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+require('@babel/register');
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const db = require('./db/models');
+const config = require('./config/config');
+
+const app = express();
+const PORT = process.env.PORT ?? 4000;
+config(app);
+
+app
+  .listen(PORT)
+  .on('error', (error) => {
+    console.error('Ошибка при запуске веб-сервера');
+    console.error(error.message);
+  })
+  .on('listening', async () => {
+    console.log('Веб-сервер слушает порт,', PORT);
+    try {
+      await db.sequelize.authenticate({ logging: false });
+      console.log('БД подключена успешно');
+    } catch (error) {
+      console.error('Ошибка подключения БД');
+      console.error(error.message);
+    }
+  });
---
+++ backend/config/config.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const sessionConfig = require('./sessions');
+const ssr = require('../middleware/ssr');
+
+const serverConfig = (app) => {
+  app.use(morgan('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use(session(sessionConfig));
+  app.use(ssr);
+};
+
+module.exports = serverConfig;
