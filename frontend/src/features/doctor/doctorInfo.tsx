b42daa5bfab62f1fe55c6ce146e9ea1e31273bbd
Pavel <pavel.bezugliy@mail.ru>
2023-02-20 19:12:28 +0500

    stylesForDoctorPage

--- frontend/src/features/doctor/doctorInfo.tsx
+++ frontend/src/features/doctor/doctorInfo.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { NavLink, useNavigate, useParams } from "react-router-dom";
 import { RootState } from "../../store";
 import { Doctor } from "./Type/type";
+import "./DoctorInfo.scss";
 
 const DoctorInfo = (): JSX.Element => {
   const { doctorId } = useParams();
@@ -22,25 +23,30 @@ const DoctorInfo = (): JSX.Element => {
   return (
     <>
       {doc && (
-        <div className="doctorDIv">
-          <div>{doc.name}</div>
-          <div>{doc.specific}</div>
-          <div className="photoDotcorDiv">
-            <img src={doc.img}></img>
-          </div>
-          <div>
-            Тут будет лежать какая-то дополнительная информация о докторе - Где
-            родился, где вырос, где учился, ученые степени и т.д.. Ее можно
-            прописать в сидах info, а пока что там есть только ----
-            {doc.info}
-            {/* {docService.map((el) => el.title)} */}
-            <div>
-              Он обучен специальностям:
-              {docService?.map((el) => el.service.title)}
+        <div className="allDoctor__container container">
+          <div className="doctor__Item">
+            <div className="doctor__Photo">
+              <img className="doctor__Img" src={doc.img}></img>
             </div>
-            <button onClick={() => navigation(-1)} type="button">
-              Вернуться ко всем врачам
-            </button>
+            <div className="doctor__Info">
+              <div>{doc.name}</div>
+              <div>{doc.specific}</div>
+
+              <div>
+                Тут будет лежать какая-то дополнительная информация о докторе -
+                Где родился, где вырос, где учился, ученые степени и т.д.. Ее
+                можно прописать в сидах info, а пока что там есть только ----
+                {doc.info}
+                {/* {docService.map((el) => el.title)} */}
+                <div>
+                  Он обучен специальностям:
+                  {docService?.map((el) => el.service.title)}
+                </div>
+                <button onClick={() => navigation(-1)} type="button">
+                  Вернуться ко всем врачам
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       )}
