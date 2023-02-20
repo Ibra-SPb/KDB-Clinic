69e85e683c7cda3591cc1d45365270157600be20
Vasilev Artyom <barkqaqa@gmail.com>
2023-02-20 19:57:52 +0300

    navbar component

--- frontend/src/features/Navbar/Navbar.tsx
+++ frontend/src/features/Navbar/Navbar.tsx
@@ -1,49 +1,62 @@
-
 import { NavLink, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
-
-
-import {logoutUser} from '../auth/authSlice'
+import { logoutUser } from '../auth/authSlice';
 import { useAppDispatch } from '../../store';
-
-
+import './NavBarStyles.scss';
 
 function Navbar(): JSX.Element {
-  const { user } = useSelector((store: RootState) => store.userState);
+	const { user } = useSelector((store: RootState) => store.userState);
 
-  const dispatch = useAppDispatch();
+	const dispatch = useAppDispatch();
 
+	const handlelogout = (): void => {
+		dispatch(logoutUser());
+	};
 
-  
-  const  handlelogout= ():void=> {
-    dispatch(logoutUser());
-    };
+	return (
+		<>
+			<div className='nav__container'>
+				<div className='nav__list'>
+					{'email' in user ? (
+						<>
+							<div>KDN clinic</div>
+						</>
+					) : (
+						<>
+							<div className='KDN_title'>
+								<NavLink to={'/'}>
+									<div>KDN clinic</div>
+								</NavLink>
+							</div>
+							<div className='menu_buttons'>
+								<div className='KDN_title'>
+									<NavLink to={'/services'}>
+										<button>Услуги</button>
+									</NavLink>
+								</div>
+								<div className='KDN_title'>
+									<NavLink to={'/doctors'}>
+										<button type='button'>Врачи</button>
+									</NavLink>
+								</div>
+								<div className='KDN_title'>Контакты</div>
+							</div>
 
-  return (
-    <>
-      <div className="nav__container">
-        <ul className="nav__list">
-          {('email' in user) ? (
-            <>
-            <li>
-            </li>
-            <li>Hi</li>
-            </>
-          ) : (
-            <>
-            <li>
-              <NavLink className="nav__list-item" to="/enterPage">
-               <button>Личный кабинет</button>
-              </NavLink>
-            </li>
-          </>
-          )}
-        </ul>
-      </div>
-      <Outlet />
-    </>
-  );
+							<div className='lk_style'>
+								<NavLink
+									className='nav__list-item'
+									to={'/enterPage'}>
+									<button type='button'>Личный кабинет</button>
+								</NavLink>
+							</div>
+						</>
+					)}
+				</div>
+			</div>
+			<Outlet />
+		</>
+	);
 }
 
 export default Navbar;
