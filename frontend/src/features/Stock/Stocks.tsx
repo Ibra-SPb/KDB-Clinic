74fea1aa1baf79fd3c777bb91f825e254deace49
Ibra <Ibrahim-1991@mail.ru>
2023-02-21 16:16:40 +0300

    fix bugs

--- frontend/src/features/Stock/Stocks.tsx
+++ frontend/src/features/Stock/Stocks.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import { RootState } from '../../store';
 import './stock.css';
 
