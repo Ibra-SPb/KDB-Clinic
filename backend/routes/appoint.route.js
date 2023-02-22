abab4a1269cc8f0953856748b958bc9b9dde6c84
Vasilev Artyom <barkqaqa@gmail.com>
2023-02-22 14:40:29 +0300

    all done

--- backend/routes/appoint.route.js
+++ backend/routes/appoint.route.js
@@ -1,16 +1,20 @@
 const router = require('express').Router();
-const { Visit, Doctor, Service, Service_Doctor, User } = require('../db/models');
-
+const {
+  Visit,
+  Doctor,
+  Service,
+  Service_Doctor,
+  User,
+} = require('../db/models');
+const TelegramBot = require('node-telegram-bot-api');
 router.get('/visit', async (req, res) => {
   try {
-    const visits = await Visit.findAll(
-      {
-        include: [
-          { model: Doctor, as: 'doctor' },
-          { model: Service, as: 'service' }
-        ]
-      }
-    );
+    const visits = await Visit.findAll({
+      include: [
+        { model: Doctor, as: 'doctor' },
+        { model: Service, as: 'service' },
+      ],
+    });
     res.status(200).json(visits);
   } catch ({ message }) {
     res.status(500).json(message);
@@ -19,14 +23,12 @@ router.get('/visit', async (req, res) => {
 
 router.get('/table', async (req, res) => {
   try {
-    const visits = await Service_Doctor.findAll(
-      {
-        include: [
-          { model: Doctor, as: 'doctor' },
-          { model: Service, as: 'service' }
-        ]
-      }
-    );
+    const visits = await Service_Doctor.findAll({
+      include: [
+        { model: Doctor, as: 'doctor' },
+        { model: Service, as: 'service' },
+      ],
+    });
     res.status(200).json(visits);
   } catch ({ message }) {
     res.status(500).json(message);
@@ -37,53 +39,76 @@ router.post('/', async (req, res) => {
   try {
     const { service, doctor } = req.body;
     // console.log(service, doctor)
-    let visits = await Visit.findAll(
-      {
-        include: [
-          { model: Doctor, as: 'doctor' },
-          { model: Service, as: 'service' }
-        ]
-      }
-    );
-    const time = ['09:00', '10:00', '11:00', '12:00', '14:00', '15:00', '16:00']
-    const date = new Date()
+    let visits = await Visit.findAll({
+      include: [
+        { model: Doctor, as: 'doctor' },
+        { model: Service, as: 'service' },
+      ],
+    });
+    const time = [
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '14:00',
+      '15:00',
+      '16:00',
+    ];
+    const date = new Date();
     let arrDate = [];
 
     for (let i = 1; i <= 7; i++) {
-      arrDate.push({ date: new Date(date.getFullYear(), date.getMonth(), date.getDate() + i), time: time })
+      arrDate.push({
+        date: new Date(date.getFullYear(), date.getMonth(), date.getDate() + i),
+        time: time,
+      });
     }
 
     arrDate.forEach((ad) => {
-      visits.filter((vs) => vs.doctor.name === doctor).forEach((vs) => {
-        if (vs.date.toLocaleString() === ad.date.toLocaleString()) {
-          ad.time = ad.time.filter((tm) => tm !== vs.time)
-        } else {
-        }
-      })
-    })
+      visits
+        .filter((vs) => vs.doctor.name === doctor)
+        .forEach((vs) => {
+          if (vs.date.toLocaleString() === ad.date.toLocaleString()) {
+            ad.time = ad.time.filter((tm) => tm !== vs.time);
+          } else {
+          }
+        });
+    });
 
-    res.status(200).json({ arrDate })
-
+    res.status(200).json({ arrDate });
   } catch ({ message }) {
     res.status(500).json(message);
   }
-})
+});
 
 router.post('/create', async (req, res) => {
   try {
     const { doctor, service, date, time } = req.body;
 
-    const doc = await Doctor.findOne({ where: { name: doctor } })
-    const ser = await Service.findOne({ where: { title: service } })
-    const user = await User.findOne({ where: { id: req.session.userId } })
+    const doc = await Doctor.findOne({ where: { name: doctor } });
+    const ser = await Service.findOne({ where: { title: service } });
+    const user = await User.findOne({ where: { id: req.session.userId } });
 
-    const visit = await Visit.create({ userId: user.id, doctorId: doc.id, serviceId: ser.id, date, time, status: true })
+    const visit = await Visit.create({
+      userId: user.id,
+      doctorId: doc.id,
+      serviceId: ser.id,
+      date,
+      time,
+      status: true,
+    });
+    const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+    await bot.sendMessage(
+      202606326,
+      `Новая запись:\nДоктор: ${doctor}\nУслуга: ${service}\nДата: ${date}\nВремя: ${time}`
+    );
+    await bot.stopPolling();
     if (visit) {
-      res.status(200).json({ status: true })
+      res.status(200).json({ status: true });
     }
   } catch ({ message }) {
     res.status(500).json(message);
   }
-})
+});
 
 module.exports = router;
