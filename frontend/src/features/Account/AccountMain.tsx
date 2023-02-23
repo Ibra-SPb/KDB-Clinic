7e81f20693643ba3759aa1a35b7af175ea2b80cf
Ibra <Ibrahim-1991@mail.ru>
2023-02-23 10:59:06 +0300

    account fix

--- frontend/src/features/Account/AccountMain.tsx
+++ frontend/src/features/Account/AccountMain.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { useAppDispatch } from '../../store';
-import { logoutUser } from '../auth/authSlice';
+import { RootState } from '../../store';
 import AccountHeader from './AccountHeader';
 
 export default function AccountMain(): JSX.Element {
-  const dispatch = useAppDispatch();
-
-  const handlelogout = (): void => {
-    dispatch(logoutUser());
-  };
+  const { user } = useSelector((store: RootState) => store.userState);
 
   return (
     <div className="account__container">
       <AccountHeader />
       <div className="account__main">
-        <NavLink to="/account/profile">Профиль</NavLink>
-        <NavLink to="/account/visits">Приёмы</NavLink>
-        <NavLink to="/account/results">Результаты</NavLink>
-        <NavLink className="nav__list-item" to="/logout">
-          <button type="button" onClick={handlelogout}>
-            Logout
-          </button>
+        <NavLink to="/account/profile">
+          <button type="button">Профиль</button>
         </NavLink>
+        <NavLink to="/account/visits"><button type="button">Приёмы</button></NavLink>
+        <NavLink to="/account/results"><button type="button">Результаты</button></NavLink>
+        {'email' in user && user.name === 'admin' && user.id === 1 && (
+          <NavLink to="/account/upload"><button type="button">Загрузка результатов</button></NavLink>
+        )}
       </div>
     </div>
   );
