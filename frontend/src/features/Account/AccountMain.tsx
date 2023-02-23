8af2b0ecf39ea65e3f281edb9d3456117229e4b8
Ibra <Ibrahim-1991@mail.ru>
2023-02-23 17:51:05 +0300

    add conditional rendering

--- frontend/src/features/Account/AccountMain.tsx
+++ frontend/src/features/Account/AccountMain.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import { RootState } from '../../store';
 import AccountHeader from './AccountHeader';
+import AccountResults from './AccountResults';
+import AccountUpload from './AccountUpload';
+import AccountVisits from './AccountVisits';
+import Profile from './Profile';
 
 export default function AccountMain(): JSX.Element {
+  const [page, setPage] = useState('Профиль');
   const { user } = useSelector((store: RootState) => store.userState);
 
   return (
     <div className="account__container">
       <AccountHeader />
       <div className="account__main">
-        <NavLink to="/account/profile">
-          <button type="button">Профиль</button>
-        </NavLink>
-        <NavLink to="/account/visits"><button type="button">Приёмы</button></NavLink>
-        <NavLink to="/account/results"><button type="button">Результаты</button></NavLink>
+        <button onClick={() => setPage('Профиль')} type="button">
+          Профиль
+        </button>
+        <button onClick={() => setPage('Приёмы')} type="button">Приёмы</button>
+        <button onClick={() => setPage('Результаты')} type="button">Результаты</button>
         {'email' in user && user.name === 'admin' && user.id === 1 && (
-          <NavLink to="/account/upload"><button type="button">Загрузка результатов</button></NavLink>
+          <button onClick={() => setPage('Загрузка результатов')} type="button">Загрузка результатов</button>
         )}
       </div>
+      <div>{page === 'Профиль' && <Profile />}</div>
+      <div>{page === 'Приёмы' && <AccountVisits />}</div>
+      <div>{page === 'Результаты' && <AccountResults />}</div>
+      <div>{page === 'Загрузка результатов' && <AccountUpload />}</div>
     </div>
   );
 }
